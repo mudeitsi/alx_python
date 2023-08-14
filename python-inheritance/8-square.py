b940915cf67186a5541a4a2bf6a946f55316c756
mudeitsi <eserenash@gmail.comm>
2023-08-14 07:03:02 -0700

    corrected task 8

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
-
-
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """
+    Class representing a Square, which inherits from the Rectangle class.
+    """
     def __init__(self, size):
-        """ size init"""
-        self.__size = size
-        super().__init__(self.__size, self.__size)
+        """
+        Initialize the Square object with the given size.
 
-    def __str__(self):
-        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
+        Args:
+            size: The length of the sides of the square.
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
