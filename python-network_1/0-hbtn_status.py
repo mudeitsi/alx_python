00d7d11d6e4d46614c1db6ab585b925d085821b7
mudeitsi <eserenash@gmail.comm>
2023-08-13 20:00:22 -0700

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
+#!usr/bin/python3
 import requests
 
-def fetch_and_display_status(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
 
-        print("Status:")
-        print("\t- Response Code:", response.status_code)
-        print("\t- Body:")
-        print("\t\t", response.text)
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    fetch_and_display_status(url)
+if response.status_code == 200:
+    data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print("Error: Unable to fetch data from the URL. Status code:", response.status_code)
 
