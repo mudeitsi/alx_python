94b960904b946694370818d4ea56a8553146d039
mudeitsi <eserenash@gmail.comm>
2023-08-14 06:20:54 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+class Square:
+    """Defines a square.
+    
+    Attributes:
+        __size (int): Size of the square.
+    """
+    
+    def __init__(self, size=0):
+        """Initializes the square with a given size.
+        
+        Args:
+            size (int, optional): Size of the square. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
