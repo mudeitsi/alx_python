3cff6b3cbc30596184194bdc975875d7f9d6b9cd
mudeitsi <eserenash@gmail.comm>
2023-08-14 06:24:01 -0700

    corrected task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,18 @@
+"""This module defines a Square class with controlled size attribute."""
+
 class Square:
-    """Defines a square.
+    """Square class defines a square by size with type and value control.
     
     Attributes:
-        __size (int): Size of the square.
+        size (int): Size of the square, must be an integer and >= 0.
     """
     
     def __init__(self, size=0):
-        """Initializes the square with a given size.
+        """Initialize Square with given size.
         
         Args:
             size (int, optional): Size of the square. Defaults to 0.
-        
+            
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
