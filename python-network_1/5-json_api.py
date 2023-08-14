37475ddf6d304a91125181f8d1fb4f7f8c624096
mudeitsi <eserenash@gmail.comm>
2023-08-14 01:51:36 -0700

    task 5

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module takes a letter as a command-line argument, sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter, and
+displays the response in the specified format.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+    
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
