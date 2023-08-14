9cb785d7a43514206ab0a4a81eb57f90ddd9a9c0
mudeitsi <eserenash@gmail.comm>
2023-08-14 06:25:59 -0700

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,33 @@
+"""This module defines a Square class to represent a geometric square with controlled size attribute and a method to calculate the area."""
+
+class Square:
+    """Square class defines a square by size with type and value control, and provides a method to calculate its area.
+    
+    Attributes:
+        size (int): Size of the square, must be an integer and >= 0.
+    """
+    
+    def __init__(self, size=0):
+        """Initialize Square with given size.
+        
+        Args:
+            size (int, optional): Size of the square. Defaults to 0.
+            
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square.
+        
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
