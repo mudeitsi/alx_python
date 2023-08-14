2dd92f1da02c2d4aace10a8e7e253d166c100671
mudeitsi <eserenash@gmail.comm>
2023-08-14 01:42:58 -0700

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,27 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+This module takes a URL as a command-line argument, sends a request to the URL,
+and displays the value of the 'X-Request-Id' variable in the response header.
+"""
+
 import requests
 import sys
 
-def fetch_and_display_x_request_id(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        x_request_id = response.headers.get('X-Request-Id')
-        if x_request_id:
-            print("Value of X-Request-Id:", x_request_id)
-        else:
-            print("X-Request-Id not found in the response headers.")
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-
 if __name__ == "__main__":
-    urls = [
-        "http://0.0.0.0:5050",
-        "http://0.0.0.0:5050",
-        "http://0.0.0.0:5050"
-    ]
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
 
-    for url in urls:
-        fetch_and_display_x_request_id(url)
