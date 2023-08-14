6380af70890358ae8b75bc014d41839f2f71a653
mudeitsi <eserenash@gmail.comm>
2023-08-14 06:13:36 -0700

    added README.md and task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+class Square:
+    """Defines a square by its private attribute size."""
+
+    def __init__(self, size):
+        """Initializes the square with the given size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
+
