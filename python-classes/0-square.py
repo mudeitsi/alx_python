b769557e0a1d0e49460e54493d52fdd4bf473537
mudeitsi <eserenash@gmail.comm>
2023-08-14 06:18:30 -0700

    redid task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,19 @@
+"""
+This module defines a Square class to represent a square with a specific size.
+The size of the square is maintained as a private attribute to ensure control over its value.
+"""
+
 class Square:
     """
-    This class defines a square by its size.
-    The size attribute is private, as it is essential to the properties of the square.
-    Control over the size attribute is maintained by keeping it private.
+    This class defines a square by its size, maintaining it as a private attribute.
+    The size of the square is essential for various computations, such as area,
+    and control over its value is maintained by keeping it private.
     """
 
     def __init__(self, size):
         """
-        Initializes the Square class with a given size.
-        :param size: The size of the square.
+        Initializes the Square class with the given size.
+        :param size: The size of the square, no type/value verification is performed.
         """
         self.__size = size
 
