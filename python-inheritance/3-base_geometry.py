6f1101b174779d65b5960e4d28b4ec57d594fd23
mudeitsi <eserenash@gmail.comm>
2023-10-16 04:12:37 -0700

    corrected task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,20 @@
-#!/usr/bin/python3
-"""
-This is an empty BaseGeometry class.
-"""
+
+"""Task 3"""
 
 
-class BaseGeometry:
-    ''' Class: BaseGeometry
-    '''
-    pass
+class BaseGeometryMeta(type):
+    """Meta Class for BAseGeometry"""
+
+    def __dir__(self):
+        attributes = super().__dir__()
+        used_attr = [att for att in attributes if att != "__init_subclass__"]
+        return used_attr
+
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """This is a BaseGeometry Class"""
+
+    def __dir__(self):
+        attributes = super().__dir__()
+        used_attr = [att for att in attributes if att != "__init_subclass__"]
+        return used_attr
