eb3caa0e13ba0ae73bba7abb461c0c4b587cea6c
mudeitsi <eserenash@gmail.comm>
2023-10-16 04:20:17 -0700

    task 6 correction

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-'''
-    Implementing a Geometry class
-'''
+
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
+# class BaseGeometryMeta(type):
+#     """Meta Class for BAseGeometry"""
+
+#     def __dir__(self):
+#         attributes = super().__dir__()
+#         used_attr = [att for att in attributes if att != "__init_subclass__"]
+#         return used_attr
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+# class BaseGeometry(metaclass=BaseGeometryMeta):
+#     """BaseGeometry Class"""
+
+#     def __dir__(self) -> None:
+#         """This will control inherited attribute access"""
+#         attributes = super().__dir__()
+#         used_attr = [att for att in attributes if att != "__init_subclass__"]
+#         return used_attr
+
+#     def area(self):
+#         raise Exception("area() is not implemented")
+
+#     def integer_validator(self, name, value):
+#         if not isinstance(value, int):
+#             raise TypeError("{} must be an integer".format(name))
+#         elif value <= 0:
+#             raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    '''
-        Implements a rectangle
-    '''
+    """Rectangle SubClass"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
